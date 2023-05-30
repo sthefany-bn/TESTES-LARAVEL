85fa02c6958be1f2970ae1979c499597e8cce8f6
sthefany-bn <sthy1917@gmail.com>
2023-05-30 15:48:57 -0300

    3

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,6 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
+                    <!-- Menu -->
+                    @component('site.menu')
+                    @endcomponent
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
